0d821de82cd7edd5c317df5ab70d65cd425d5b01
claudiomontoya <claudio.montoya@biopc.cl>
2021-11-22 21:28:48 -0300

    clase 22-11-2021

---
+++ WebApplicationClientes/Controllers/ClienteController.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibraryClientes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationClientes.Models;
+
+namespace WebApplicationClientes.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        [HttpGet]
+        [Route("api/v1/clientes")]
+        public List<clientes> listar() {
+            List<clientes> listado = new List<clientes>();
+            clienteEntity clienteData = new clienteEntity();
+            DataSet data = clienteData.listadoClientes();
+
+            for (int i = 0; i < data.Tables[0].Rows.Count; i++)
+            {
+                clientes item = new clientes();
+                item.rut =  data.Tables[0].Rows[i].ItemArray[0].ToString();
+                item.nombre = data.Tables[0].Rows[i].ItemArray[1].ToString();
+                item.apellido = data.Tables[0].Rows[i].ItemArray[2].ToString();
+                item.telefono = data.Tables[0].Rows[i].ItemArray[3].ToString();
+                listado.Add(item);
+            }
+
+            return listado;
+        }
+
+    }
+}
---
+++ WebApplicationClientes/Models/clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationClientes.Models
+{
+    public class clientes
+    {
+        public string rut { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string telefono { get; set; }
+    }
+}
